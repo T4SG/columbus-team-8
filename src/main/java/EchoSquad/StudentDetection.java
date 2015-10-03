e4cbe6a16eb8e5c7ce1da592193c7290cba7d508
unknown <jd728@cornell.edu>
2015-10-03 00:40:54 -0400

    Amazingness

---
+++ src/main/java/EchoSquad/StudentDetection.java
@@ -0,0 +1,17 @@
+public class StudentDetection {
+	public boolean isStudentAtRisk(String[] studentData)
+	{
+		//Useful Student Data Metrics to check if they're performing well or not
+		
+		/*  0 - Current Semester GPA
+			1 - Days Missed Per Month
+			2 - Days Late Per Month
+			3 - Number of Detentions
+			
+			(in real life we'd look at the statistics from the LBFF data
+			and perform predictive analytics to see which features correlate
+			the most with a student graduating highschool or not.)
+		*/
+		return true;
+	}
+}
